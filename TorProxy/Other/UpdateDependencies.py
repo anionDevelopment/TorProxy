e10b22923717faec284933e16bbc6d145d731888
Marius Göcke <marius.goecke@gmail.com>
2026-04-06 20:59:49 +0200

    updated script

--- TorProxy/Other/UpdateDependencies.py
+++ TorProxy/Other/UpdateDependencies.py
@@ -1,19 +1,19 @@
 from pathlib import Path
-import os
-from functools import cmp_to_key
 from ScriptCollection.ScriptCollectionCore import ScriptCollectionCore
 from ScriptCollection.GeneralUtilities import GeneralUtilities
 from ScriptCollection.TFCPS.TFCPS_Tools_General import TFCPS_Tools_General
 from ScriptCollection.TFCPS.Docker.TFCPS_CodeUnitSpecific_Docker import TFCPS_CodeUnitSpecific_Docker_Functions,TFCPS_CodeUnitSpecific_Docker_CLI
+from ScriptCollection.OCIImages.OCIImageManager import OCIImageManager
 
 
 def update_dependencies():
     script_file = str(Path(__file__).absolute())
     sc = ScriptCollectionCore()
     tf:TFCPS_CodeUnitSpecific_Docker_Functions=TFCPS_CodeUnitSpecific_Docker_CLI.parse(__file__)
-    image_definitions_csv_file=GeneralUtilities.normalize_path(f"{tf.get_repository_folder()}/.ScriptCollection/OCIImages/ImageDefinition.csv")
-    debian_tag=[f for f in GeneralUtilities.read_nonempty_lines_from_file(image_definitions_csv_file) if f.startswith("Debian;")][0].split(";")[2]
-    TFCPS_Tools_General(sc).update_dependency_in_resources_folder(script_file, "Tor", sc.get_latest_apt_package_version_in_debian( f"debian:{debian_tag}", "tor"))
+    oci_image_manager:OCIImageManager=OCIImageManager(sc)
+    image=oci_image_manager.get_registry_address_for_image_with_default_tag(tf.get_repository_folder(),"Debian",strict_mode=False)
+    tg=TFCPS_Tools_General(sc)
+    tg.update_dependency_in_resources_folder(script_file, "Tor", sc.get_latest_apt_package_version_in_debian( image, "tor"))
 
 
 if __name__ == "__main__":
